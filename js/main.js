b452f8b6beccbe8dcc7ee69171cffe15ece1bb1c
Kushal <kushaldeveloper@gmail.com>
2018-06-12 16:56:34 -0400

    date added

--- js/main.js
+++ js/main.js
@@ -7,10 +7,13 @@ x = y.toString();
 let myParagraph = document.getElementById("apple");
 myParagraph.innerText = "hello there";
 let step = 0;
-for (step = 0; step < 1000000; step++) {
-    window.setInterval(function(){
-        myParagraph.innerText = step.toString();
-        console.log(step.toString());
-    }, 1000);
-}
+let sravan = new Date();
 
+var a = function(){
+    setInterval(function(){
+      myParagraph.innerText = new Date();
+    },1000);
+        
+    };
+
+    a();
