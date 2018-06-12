25dd5b471b735561f402c90af7d74ae27c414a44
Kushal <kushaldeveloper@gmail.com>
2018-06-12 16:39:51 -0400

    WIP my for loop does not work

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@
 let x = "five";
 let y = 10;
 x = y.toString();
+
+let myParagraph = document.getElementById("apple");
+myParagraph.innerText = "hello there";
+let step = 0;
+for (step = 1000; step < 1000000; step++) {
+    window.setTimeout(function(){
+        console.log(step.toString());
+    }, step);
+}
+
